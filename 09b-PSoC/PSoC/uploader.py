159dc6c98648d9b1b5ae2536f93540c19bf605dd
X. Wang <xuyi@tetra.nl>
2022-07-15 07:36:45 +0200

    fixed: python

--- 09b-PSoC/PSoC/uploader.py
+++ 09b-PSoC/PSoC/uploader.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python2.7
-from serial import Serial
 import time
+import os
 from struct import pack
 from builtins import bytes
+from serial import Serial
 
 class Avr109(object):
     """AVR109 firmware upload protocol.  This currently just implements the
@@ -10,21 +11,21 @@ class Avr109(object):
     Atmega 328P used on the Mobilinkd TNC.
     """
     
-    def __init__(self, reader, writer):
-        self.sio_reader = writer
-        self.sio_writer = writer
+    def __init__(self, reader):
+        self.start()
         
     def start(self):
-        self.sio_writer.write(b'\033')
-        self.sio_writer.flush()
-        #buf = self.sio_reader.read(10)
+        os.system("stty -f /dev/cu.usbmodem101 1200")
+        print("starting")
+        time.sleep(2.5)
+        self.sio = Serial('/dev/cu.usbmodem101', 115200)
     
     def send_address(self, address):
         address //= 2 # convert from byte to word address
         ah = (address & 0xFF00) >> 8
         al = address & 0xFF
-        self.sio_writer.write(bytes(pack('cBB', b'A', ah, al)))
-        self.sio_writer.flush()
+        self.sio.write(bytes(pack('cBB', b'A', ah, al)))
+        self.sio.flush()
         self.verify_command_sent("Set address to: %04x" % address)
     
     def send_block(self, memtype, data):
@@ -43,9 +44,9 @@ class Avr109(object):
         ah = 0
         al = len(data)
         
-        self.sio_writer.write(bytes(pack('cBBc', b'B', ah, al, memtype)))
-        self.sio_writer.write(data)
-        self.sio_writer.flush()
+        self.sio.write(bytes(pack('cBBc', b'B', ah, al, memtype)))
+        self.sio.write(data)
+        self.sio.flush()
         self.verify_command_sent("Block load: %d" % len(data))
     
     def read_block(self, memtype, size):
@@ -59,10 +60,10 @@ class Avr109(object):
             'F' is for flash.
         @param size is the size of the block to read."""
                 
-        self.sio_writer.write(bytes(pack('cBBc', b'g', 0, size, memtype)))
-        self.sio_writer.flush()
-        self.sio_reader.timeout = 1
-        result = self.sio_reader.read(size)
+        self.sio.write(bytes(pack('cBBc', b'g', 0, size, memtype)))
+        self.sio.flush()
+        self.sio.timeout = 1
+        result = self.sio.read(size)
         return result
         
     def write_block(self, address, memtype, data):
@@ -72,10 +73,10 @@ class Avr109(object):
         pass
     
     def verify_command_sent(self, cmd):
-        timeout = self.sio_reader.timeout
-        self.sio_reader.timeout = 1
-        c = self.sio_reader.read(1)
-        self.sio_reader.timeout = timeout
+        timeout = self.sio.timeout
+        self.sio.timeout = 1
+        c = self.sio.read(1)
+        self.sio.timeout = timeout
         if c != b'\r':
             # Do not report c because it could be None
             raise IOError("programmer did not respond to command: %s" % cmd)
@@ -85,55 +86,54 @@ class Avr109(object):
     
     def chip_erase(self):
         time.sleep(.1)
-        self.sio_writer.write(b'e')
-        self.sio_writer.flush()
+        self.sio.write(b'e')
+        self.sio.flush()
         self.verify_command_sent("chip erase")
     
     def enter_program_mode(self):
         time.sleep(.1)
-        self.sio_writer.write(b'P')
-        self.sio_writer.flush()
+        self.sio.write(b'P')
+        self.sio.flush()
         #self.verify_command_sent("enter program mode")
         
     def leave_program_mode(self):
         time.sleep(.1)
-        self.sio_writer.write(b'L')
-        self.sio_writer.flush()
+        self.sio.write(b'L')
+        self.sio.flush()
         self.verify_command_sent("leave program mode")
    
     def exit_bootloader(self):
         time.sleep(.1)
-        self.sio_writer.write(b'E')
-        self.sio_writer.flush()
+        self.sio.write(b'E')
+        self.sio.flush()
         self.verify_command_sent("exit bootloader")
         
     def supports_auto_increment(self):
         # Auto-increment support
         time.sleep(.1)
-        self.sio_writer.write(b'a')
-        self.sio_writer.flush()
-        return (self.sio_reader.read(1) == b'Y')
+        self.sio.write(b'a')
+        self.sio.flush()
+        return (self.sio.read(1) == b'Y')
     
     def get_block_size(self):
         time.sleep(.1)
-        self.sio_writer.write(b'b')
-        self.sio_writer.flush()
-        if self.sio_reader.read(1) == b'Y':
-            tmp = bytearray(self.sio_reader.read(2))
+        self.sio.write(b'b')
+        self.sio.flush()
+        if self.sio.read(1) == b'Y':
+            tmp = bytearray(self.sio.read(2))
             return tmp[0] * 256 + tmp[1]
         return 0
    
     def get_bootloader_signature(self):
-        self.sio_reader.timeout = 1
-        junk = self.sio_reader.read()
-        self.sio_reader.timeout = .1
-        for i in range(20):
+        self.sio.timeout = 1
+        junk = self.sio.read()
+        self.sio.timeout = .1
+        for i in range(10):
             self.start()
             # Bootloader
-            self.sio_writer.write(b'S')
-            self.sio_writer.flush()
-            loader = self.sio_reader.read(7)
-            print(loader)
+            self.sio.write(b'S')
+            self.sio.flush()
+            loader = self.sio.read(7)
             if loader == b'AVRBOOT':
                 return loader
             time.sleep(.1)
@@ -141,12 +141,12 @@ class Avr109(object):
     
     def send_expect(self, cmd, expected, retries = 5):
         expected_len = len(expected)
-        self.sio_reader.timeout = .1
-        junk = self.sio_reader.read()
+        self.sio.timeout = .1
+        junk = self.sio.read()
         for i in range(retries):
-            self.sio_writer.write(cmd)
-            self.sio_writer.flush()
-            received = bytes(self.sio_reader.read(expected_len))
+            self.sio.write(cmd)
+            self.sio.flush()
+            received = bytes(self.sio.read(expected_len))
             if received == expected:
                 return True
             time.sleep(.1)
@@ -158,10 +158,10 @@ class Avr109(object):
         MAJOR.MINOR (e.g. "1.7")."""
 
         time.sleep(.1)
-        self.sio_writer.write(b'V')
-        self.sio_writer.flush()
-        self.sio_reader.timeout = .1
-        sw_version = bytearray(bytes(self.sio_reader.read(2), encoding='utf8'))
+        self.sio.write(b'V')
+        self.sio.flush()
+        self.sio.timeout = .1
+        sw_version = bytes(self.sio.read(2))
         if len(sw_version) < 2: return "unknown"
         return "%d.%d" % (sw_version[0] - 48, sw_version[1] - 48)
     
@@ -176,12 +176,12 @@ class Avr109(object):
     
     def get_device_list(self):
         # Device Code
-        self.sio_reader.timeout = .1
-        self.sio_writer.write(b't')
-        self.sio_writer.flush()
+        self.sio.timeout = .1
+        self.sio.write(b't')
+        self.sio.flush()
         device_list = []
         while True:
-            device = bytearray(self.sio_reader.read(1))
+            device = self.sio.read(1)
             if device[0] == 0: break
             device_list.append(device)
         
@@ -189,30 +189,16 @@ class Avr109(object):
         
     def get_device_signature(self):
         # Read Signature
-        self.sio_writer.write(b's')
-        self.sio_writer.flush()
-        return self.sio_reader.read(3)
-f = open("build/psoc.hex", "r")
+        self.sio.write(b's')
+        self.sio.flush()
+        return self.sio.read(3)
 
-import os
-print("ready")
-#ser = Serial('/dev/cu.usbmodem101', 11520)
-#print(ser.readline())
-while True:
-    if os.path.exists("/dev/cu.usbmodem101"):
-        break
-    else:
-        break
-print("reset")
-while True:
-    if os.path.exists("/dev/cu.usbmodem101"):
-        break
-    else:
-        pass
 #os.system("make upload")
-print("ok")
-ser = Serial('/dev/cu.usbmodem101', 115200)
-programmer = Avr109(f, ser)
-programmer.start()
+programmer = Avr109(open("build/psoc.hex", "r"))
 #programmer.enter_program_mode()
 print(programmer.get_bootloader_signature())
+print(programmer.get_device_signature())
+print(programmer.get_software_version())
+print(programmer.get_programmer_type())
+print(programmer.get_device_list())
+print(programmer.get_block_size())
